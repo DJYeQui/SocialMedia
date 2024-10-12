a452513d3246c4b733efd2a870d8db5fb0ce75b5
Liande <fatih.anamasli@std.izmirekonomi.edu.tr>
2024-10-12 14:18:46 +0300

    uı deneme

---
+++ src/main/java/swingUi/login.java
@@ -0,0 +1,12 @@
+package swingUi;
+
+import javax.swing.*;
+
+public class login {
+    private JLabel Header;
+    private JButton button1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
