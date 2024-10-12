1c59080e4686da5a77117fae6e467d812ba64059
Liande <fatih.anamasli@std.izmirekonomi.edu.tr>
2024-10-12 15:14:47 +0300

    clean

--- src/main/java/com/example/socialmedia/entity/UserEntity.java
+++ src/main/java/com/example/socialmedia/entity/UserEntity.java
@@ -6,10 +6,6 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.Value;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
